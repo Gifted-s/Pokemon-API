a689274b7facc926443a7fab44e8cdaae158d2ce
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-10 18:42:25 +0100

    format code

--- Models/models.go
+++ Models/models.go
@@ -1,36 +1,35 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Pokemon struct {
-	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name       string             `json:"name" bson:"name,omitempty"`
-	Type1      string             `json:"type1" bson:"type1,omitempty"`
-	Type2      string             `json:"type2" bson:"type2,omitempty"`
-	Total      int                `json:"total" bson:"total,omitempty"`
-	HP         int                `json:"hp" bson:"hp,omitempty"`
-	Attack     int                `json:"attack" bson:"attack ,omitempty"`
-	Defence    int                `json:"defence" bson:"defence ,omitempty"`
-	AttackSpeed   int             `json:"attackSpeed" bson:"attackSpeed ,omitempty"`
-	DefenceSpeed  int             `json:"defenceSpeed" bson:"defenceSpeed ,omitempty"`
-	Speed      int                `json:"speed" bson:"speed ,omitempty"`
-	Generation int                `json:"generation" bson:"generation ,omitempty"`
-	Lengendary bool               `json:"lengendary" bson:"lengendary ,omitempty"`
+	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name         string             `json:"name" bson:"name,omitempty"`
+	Type1        string             `json:"type1" bson:"type1,omitempty"`
+	Type2        string             `json:"type2" bson:"type2,omitempty"`
+	Total        int                `json:"total" bson:"total,omitempty"`
+	HP           int                `json:"hp" bson:"hp,omitempty"`
+	Attack       int                `json:"attack" bson:"attack ,omitempty"`
+	Defence      int                `json:"defence" bson:"defence ,omitempty"`
+	AttackSpeed  int                `json:"attackSpeed" bson:"attackSpeed ,omitempty"`
+	DefenceSpeed int                `json:"defenceSpeed" bson:"defenceSpeed ,omitempty"`
+	Speed        int                `json:"speed" bson:"speed ,omitempty"`
+	Generation   int                `json:"generation" bson:"generation ,omitempty"`
+	Lengendary   bool               `json:"lengendary" bson:"lengendary ,omitempty"`
 }
 
-
 type Configuration struct {
-    Db_Config  Db_Config_Struct
+	Db_Config Db_Config_Struct
 }
 
 type Db_Config_Struct struct {
 	Pokemon_Collection_Name string
-	Db_Name string
+	Db_Name                 string
 }
 
 type Collections struct {
-	PokeMons  *mongo.Collection
+	PokeMons *mongo.Collection
 }
--- configs/configs.go
+++ configs/configs.go
@@ -3,16 +3,12 @@ package configs
 import (
 "pokemon/m/v1/models")
 
-
-
-// getting env
 var Config = models.Configuration {
 
 	Db_Config :models.Db_Config_Struct{
 	Pokemon_Collection_Name:"pokemons",
 	Db_Name:"pokemon_db",
     },
-
 
 }
 
