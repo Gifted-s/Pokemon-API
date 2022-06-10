440b6d8b8f2eb89830e51dceeb9d96af89d2cad5
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-10 17:52:16 +0100

    updated db uri variable name

--- database/connection.go
+++ database/connection.go
@@ -13,10 +13,10 @@ import (
 func ConnectMongoDB() *mongo.Client {
 	var dbName = os.Getenv("DB_NAME")
     var dbPassword = os.Getenv("DB_PASSWORD")
-	
-	URI :="mongodb+srv://"+dbName+":"+dbPassword+"@cluster0.jciel9m.mongodb.net/?retryWrites=true&w=majority"
+
+	dbConnectionURI :="mongodb+srv://"+dbName+":"+dbPassword+"@cluster0.jciel9m.mongodb.net/?retryWrites=true&w=majority"
 	// Set client options
-	clientOptions := options.Client().ApplyURI(URI)
+	clientOptions := options.Client().ApplyURI(dbConnectionURI)
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
