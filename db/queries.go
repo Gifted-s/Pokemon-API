dd0bb1e7a3165726791a2f106ec299d3933d2f59
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-11 11:04:56 +0100

    added Key and Value Literal to matchStage for readability

--- db/queries.go
+++ db/queries.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"time"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,11 +13,15 @@ func GetPokeMons(queryBody map[string][]string) ([]primitive.M, error) {
 	pokemonCollection := GetDBCollections().PokeMons
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	fmt.Print(queryBody["search"][0])
-	matchStage := bson.D{{"$search", bson.D{{"index", "nameSearchIndex"}, {"text", bson.M{
-		"query": queryBody["search"][0],
-		"path":  "name",
-		"fuzzy": bson.M{},
-	}}}}}
+	matchStage := bson.D{
+		primitive.E{Key: "$search",
+			Value: bson.D{
+				primitive.E{Key: "index", Value: "nameSearchIndex"},
+				primitive.E{Key: "text", Value: bson.M{
+					"query": queryBody["search"][0],
+					"path":  "name",
+					"fuzzy": bson.M{},
+				}}}}}
 	pokeMonInfoCursor, err := pokemonCollection.Aggregate(ctx, mongo.Pipeline{matchStage})
 	if err != nil {
 		return []primitive.M{}, err
