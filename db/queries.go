bbc9441633a65436a61c5734077ecf09d4936555
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-13 12:36:49 +0100

    removed unneccesary condition for get pokemon query

--- db/queries.go
+++ db/queries.go
@@ -4,22 +4,17 @@ import (
 	"context"
 	"pokemon/m/v1/models"
 	"time"
-
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetPokeMons(queryBody map[string][]string) ([]models.Pokemon, error) {
-	var pokeMonInfoCursor *mongo.Cursor
 	var err error
 	pokemonCollection := GetDBCollections().PokeMons
 	queryFormat := GetPokemonQueryFormatter(queryBody)
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	if queryBody["hp"] != nil && queryBody["defense"] != nil && queryBody["attack"] != nil {
-		pokeMonInfoCursor, err = pokemonCollection.Find(ctx,
-			bson.M{"$and": queryFormat.QueryCondition},
-			queryFormat.Options)
-	}
+	pokeMonInfoCursor, err := pokemonCollection.Find(ctx,
+		bson.M{"$and": queryFormat.QueryCondition},
+		queryFormat.Options)
 	if err != nil {
 		return []models.Pokemon{}, err
 	}
