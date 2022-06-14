006fa683102a4511270dc0ff33a2e2b1372fa12d
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-14 23:56:38 +0100

    wrote test for construct_pokemon_slice

---
+++ tests/unit_tests/construct_pokemon_slice/construct_pokemon_slice_test.go
@@ -0,0 +1,90 @@
+package construct_pokemon_slice_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"pokemon/m/v1/helpers"
+	"pokemon/m/v1/models"
+	"reflect"
+	"testing"
+)
+
+func TestConstructPokemonSlice(t *testing.T) {
+	assert := assert.New(t)
+	fakeId1, _ := primitive.ObjectIDFromHex("62a705c3cfb07e0c463b315b")
+	fakeId2, _ := primitive.ObjectIDFromHex("62a705c5cfb07e0c463b315c")
+
+	fakePokemanSliceWithEditDistance := []models.PokemonsWithEditDistanceStruct{
+		{EditDistance: 6,
+			Pokemon: models.Pokemon{
+				ID: fakeId1,
+				Name:         "Charmander",
+				Type1:        "Fire",
+				Type2:        "Grass",
+				Total:        100,
+				HP:           50,
+				Attack:       400,
+				Defense:      200,
+				AttackSpeed:  600,
+				DefenseSpeed: 10,
+				Speed:        90,
+				Generation:   1,
+				Lengendary:   false},
+		},
+		{EditDistance: 8,
+			Pokemon: models.Pokemon{
+				ID: fakeId2,
+				Name:         "Ceramic",
+				Type1:        "Fire",
+				Type2:        "Grass",
+				Total:        100,
+				HP:           50,
+				Attack:       456,
+				Defense:      54,
+				AttackSpeed:  600,
+				DefenseSpeed: 10,
+				Speed:        90,
+				Generation:   1,
+				Lengendary:   false},
+		},
+	}
+
+
+	expectedPokemonSlice := []*models.Pokemon{
+		{
+			
+				ID: fakeId1,
+				Name:         "Charmander",
+				Type1:        "Fire",
+				Type2:        "Grass",
+				Total:        100,
+				HP:           50,
+				Attack:       400,
+				Defense:      200,
+				AttackSpeed:  600,
+				DefenseSpeed: 10,
+				Speed:        90,
+				Generation:   1,
+				Lengendary:   false,
+		},
+		{
+				ID: fakeId2,
+				Name:         "Ceramic",
+				Type1:        "Fire",
+				Type2:        "Grass",
+				Total:        100,
+				HP:           50,
+				Attack:       456,
+				Defense:      54,
+				AttackSpeed:  600,
+				DefenseSpeed: 10,
+				Speed:        90,
+				Generation:   1,
+				Lengendary:   false,
+		},
+	}
+    sortedPokemons := helpers.PokemonSliceConstructor(fakePokemanSliceWithEditDistance)
+	slicesAreEqual := reflect.DeepEqual(expectedPokemonSlice, sortedPokemons)
+	assert.Equal(true, slicesAreEqual, "The two slices must be equal")
+
+}
